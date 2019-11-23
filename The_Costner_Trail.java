8832fcfd794d4bed1e819ea4749d4d216471bc67
Costnertk <58087296+Costnertk@users.noreply.github.com>
2019-11-23 11:29:43 -0500

    Modified gender option

--- The_Costner_Trail.java
+++ The_Costner_Trail.java
@@ -8,56 +8,25 @@ public class The_Costner_Trail
         System.out.println("Menu Page");
         System.out.println("Enter the name you want for your character");
         String name = input.nextLine();
-        System.out.println("1.Use the pronouns he, his\n2.Use the pronouns she, her");
+        System.out.println("1.Male \n2.Female");
         int choice = input.nextInt();
         if(choice == 1)
         {
           //this will probably need to be a method too to call value1 "he/she" and value2 "his/her"
-          int pronoun = 0;
-        } else {
-          //pronoun = 1;
+          int gender = 0;
+        } 
+        else 
+        {
+          //gender = 1;
         }
-        //
-        
-        
+
+
+
         
         //The cool shit
-        
-        System.out.println(" ____________________   ___     ___   ____________");
-        System.out.println("|                    | |   |   |   | |            |");
-        System.out.println("|_______     ________| |   |   |   | |   _________|");
-        System.out.println("        |   |          |   |   |   | |  |");
-        System.out.println("        |   |          |   |___|   | |  |_________");
-        System.out.println("        |   |          |           | |            |");
-        System.out.println("        |   |          |    ___    | |   _________|");
-        System.out.println("        |   |          |   |   |   | |  |");
-        System.out.println("        |   |          |   |   |   | |  |_________");
-        System.out.println("        |   |          |   |   |   | |            |");
-        System.out.println("        |___|          |___|   |___| |____________|");
-        System.out.println();
-        System.out.println(" ____________________      _________________     ____________      ____________________     ____        ____     ____________     ______________");
-        System.out.println("|                    |    |    _________    |   |            |    |                    |   |     L     |    |   |            |   |              |");
-        System.out.println("|     _______________|    |   |         |   |   |    ________|    |_______     ________|   |      L    |    |   |   _________|   |    ______    |");
-        System.out.println("|    |                    |   |         |   |   |   |                     |   |            |    |  L   |    |   |  |             |   |      |   |");
-        System.out.println("|    |                    |   |         |   |   |   |________             |   |            |    |   L  |    |   |  |_________    |   |______|   |");
-        System.out.println("|    |                    |   |         |   |   |            |            |   |            |    |L   L |    |   |            |   |          ____|");
-        System.out.println("|    |                    |   |         |   |   |________    |            |   |            |    | L   L|    |   |   _________|   |   |___   L");
-        System.out.println("|    |                    |   |         |   |            |   |            |   |            |    |  L   |    |   |  |             |   |   L   L");
-        System.out.println("|    |_______________     |   |         |   |    ________|   |            |   |            |    |   L  |    |   |  |_________    |   |    L   L");
-        System.out.println("|                    |    |   |_________|   |   |            |            |   |            |    |    L      |   |            |   |   |     L   L");
-        System.out.println("|____________________|    |_________________|   |____________|            |___|            |____|     L_____|   |____________|   |___|      L___L");
-        System.out.println("");
-        System.out.println("");
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+        //Title converted to one line print statement. Would like to get Images print object working as alternative.
+        System.out.print(" ____________________   ___     ___   ____________\n" + "|                    | |   |   |   | |            |\n" + "|_______     ________| |   |   |   | |   _________|\n" + "        |   |          |   |   |   | |  |\n" + "        |   |          |   |___|   | |  |_________\n" + "        |   |          |           | |            |\n" + "        |   |          |    ___    | |   _________|\n" + "        |   |          |   |   |   | |  |\n" + "        |   |          |   |   |   | |  |_________\n" + "        |   |          |   |   |   | |            |\n" + "        |___|          |___|   |___| |____________|\n" + "\n" + " ____________________      _________________     ____________      ____________________     ____        ____     ____________     ______________\n" + "|                    |    |    _________    |   |            |    |                    |   |     L     |    |   |            |   |              |\n" + "|     _______________|    |   |         |   |   |    ________|    |_______     ________|   |      L    |    |   |   _________|   |    ______    |\n" + "|    |                    |   |         |   |   |   |                     |   |            |    |  L   |    |   |  |             |   |      |   |\n" + "|    |                    |   |         |   |   |   |________             |   |            |    |   L  |    |   |  |_________    |   |______|   |\n" + "|    |                    |   |         |   |   |            |            |   |            |    |L   L |    |   |            |   |          ____|\n" + "|    |                    |   |         |   |   |________    |            |   |            |    | L   L|    |   |   _________|   |   |___   L\n" + "|    |                    |   |         |   |            |   |            |   |            |    |  L   |    |   |  |             |   |   L   L\n" + "|    |_______________     |   |         |   |    ________|   |            |   |            |    |   L  |    |   |  |_________    |   |    L   L\n" + "|                    |    |   |_________|   |   |            |            |   |            |    |    L      |   |            |   |   |     L   L\n" + "|____________________|    |_________________|   |____________|            |___|            |____|     L_____|   |____________|   |___|      L___L\n" + "\n" + "\n");
         
         
         System.out.println("1.Start game" + "\n" + "2.View Achievements");
@@ -65,12 +34,14 @@ public class The_Costner_Trail
         
         //This while statment will be concatonated into its own method because i expect this to be called often
         //this will have parameters for accepted values so that there can be more then two choices
-        while(choice > 2 || choice < 1){
-            System.out.println("Thats not one of the options\nEnter one of the given choices");
-            choice = input.nextInt();
-          }
-        //
+        while(choice > 2 || choice < 1)
+        {
+          System.out.println("Thats not one of the options\nEnter one of the given choices");
+          choice = input.nextInt();
+        }
         
+
+
         
         //This is the begining of tha acutal game logic
         if(choice == 1){
