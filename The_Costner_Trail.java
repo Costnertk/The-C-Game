b1d9d0668646a701a6322dd24291d5d2a40fe9f6
jdub-brickmaster <52805633+jdub-brickmaster@users.noreply.github.com>
2019-12-04 13:44:37 -0500

    Cleaning main

--- The_Costner_Trail.java
+++ The_Costner_Trail.java
@@ -43,22 +43,14 @@ public class The_Costner_Trail
           }
         }
         
-        //The cool shit
-
-        //Title converted to one line print statement. Would like to get Images print object working as alternative.
+        
         System.out.print(" ____________________   ___     ___   ____________\n" + "|                    | |   |   |   | |            |\n" + "|_______     ________| |   |   |   | |   _________|\n" + "        |   |          |   |   |   | |  |\n" + "        |   |          |   |___|   | |  |_________\n" + "        |   |          |           | |            |\n" + "        |   |          |    ___    | |   _________|\n" + "        |   |          |   |   |   | |  |\n" + "        |   |          |   |   |   | |  |_________\n" + "        |   |          |   |   |   | |            |\n" + "        |___|          |___|   |___| |____________|\n" + "\n" + " ____________________      _________________     ____________      ____________________     ____        ____     ____________     ______________\n" + "|                    |    |    _________    |   |            |    |                    |   |     L     |    |   |            |   |              |\n" + "|     _______________|    |   |         |   |   |    ________|    |_______     ________|   |      L    |    |   |   _________|   |    ______    |\n" + "|    |                    |   |         |   |   |   |                     |   |            |    |  L   |    |   |  |             |   |      |   |\n" + "|    |                    |   |         |   |   |   |________             |   |            |    |   L  |    |   |  |_________    |   |______|   |\n" + "|    |                    |   |         |   |   |            |            |   |            |    |L   L |    |   |            |   |          ____|\n" + "|    |                    |   |         |   |   |________    |            |   |            |    | L   L|    |   |   _________|   |   |___   L\n" + "|    |                    |   |         |   |            |   |            |   |            |    |  L   |    |   |  |             |   |   L   L\n" + "|    |_______________     |   |         |   |    ________|   |            |   |            |    |   L  |    |   |  |_________    |   |    L   L\n" + "|                    |    |   |_________|   |   |            |            |   |            |    |    L      |   |            |   |   |     L   L\n" + "|____________________|    |_________________|   |____________|            |___|            |____|     L_____|   |____________|   |___|      L___L\n" + "\n" + "\n");
         
         
         System.out.println("1.Start game" + "\n" + "2.View Achievements" + "\n" + "3.Continue Game");
         choice = input.nextInt();
         
-        //This while statment will be concatonated into its own method because i expect this to be called often
-        //this will have parameters for accepted values so that there can be more then two choices
-        // while(choice > 2 || choice < 1)
-        // {
-          // System.out.println("Thats not one of the options\nEnter one of the given choices");
-          // choice = input.nextInt();
-        // }
+      
         
         Quests selectedQuest = new Quests(name, gender);
 
@@ -100,17 +92,6 @@ public class The_Costner_Trail
           System.out.println("Not a valid input. Please try again");
         }
         
-        
-        //This is the begining of tha acutal game logic
-        //if(choice == 1){
-          //This is where the game is played
-         // System.out.println("It's a dark and stormy night as " + name + " drives home from work...\n1.Park car on the street\n2.Park car in garage");
-         // choice = input.nextInt();
-          
-       // } 
-        //else if(choice == 2){
-          //this accesses the achive class
-          //System.out.println("Achivements and stuff");
-        //}
+       
     }
 }
