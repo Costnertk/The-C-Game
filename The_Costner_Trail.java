ac43d19b4d3bcc746b2196a51353fc940888d0a7
jdub-brickmaster <52805633+jdub-brickmaster@users.noreply.github.com>
2019-12-05 13:48:02 -0500

    syimplification of main

--- The_Costner_Trail.java
+++ The_Costner_Trail.java
@@ -23,27 +23,13 @@ public class The_Costner_Trail
 
         if(choice == 1)
         {
-          //System.out.println("\f"); tried to implemant clear screen;
-          for (int i = 1; i < 3; i++) {
-            //                ^ this variable here will be updated as the number of quests increases
-            // a for loop is used to make the variable 'i' which is used to itterate through
-            // quests
-            if (i > 1) {
-              // this checks to see if i > 1 which means user has complete quest 1 and will be
-              // presented with a choice to continue
-              System.out.println("Do you want to continue to the next quest?\n(1)Yes\n(2)No");
-              final int continueQuest = input.nextInt();
-              if (continueQuest == 1) {
-                // this allows the user to continue playing quests until they choose no allowing
-                // them to end the game early
-               
-              }
-            } else if (i == 1) {
-              // this looks for the first iteration of i when i = 1 and starts quest 1
-              selectedQuest.runQuest(1);
-            }
+          selectedQuest.Quest1();
+          for(int i = currentQuest; i < 3; i++)
+          {
+            System.out.println("Do you want to continue to the next quest?\n(1)Yes\n(2)No");
+            final int continueQuest = input.nextInt();
+            selectedQuest.runQuest(i);
           }
-          currentQuest = 1;
         }
         else if(choice == 2)
         {
