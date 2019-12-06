d9568b6f63a5fedb5609e3c62bbcc27d3dc7e4b0
Costnertk <58087296+Costnertk@users.noreply.github.com>
2019-12-06 13:40:08 -0500

    Main does not need to acccess quest class

--- The_Costner_Trail.java
+++ The_Costner_Trail.java
@@ -1,12 +1,12 @@
 import java.util.*;
 public class The_Costner_Trail
 {
-    public static void main(String[] ars) {
-    Scanner input = new Scanner(System.in);
-    System.out.println("Menu Page");
-    System.out.println("Enter the name you want for your character");
-     String name = input.nextLine();
-    Quests questSelector = new Quests(name);
-        questSelector.runQuest(1);
+    public static void main(String[] ars) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Menu Page");
+        System.out.println("Enter the name you want for your character");
+        String name = input.nextLine();
+        
     }
 }
