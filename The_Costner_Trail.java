cd5b43ee998f4ad16374ff78f25440553112633a
jdub-brickmaster <52805633+jdub-brickmaster@users.noreply.github.com>
2019-12-05 23:34:29 -0500

    cleaned of everything but base game code

--- The_Costner_Trail.java
+++ The_Costner_Trail.java
@@ -1,48 +1,14 @@
 import java.util.*;
 public class The_Costner_Trail
 {
-  public static int currentQuest = 1;
-    
-    public static void main(String[] ars)
-    {
-        
-        Scanner input = new Scanner(System.in);
-        //These statments will be encorperated into their own method as "character details"
-        System.out.println("Menu Page");
-        System.out.println("Enter the name you want for your character");
-        String name = input.nextLine();
-        Images text = new Images(name);
-        questRunner execute = new questRunner(name);
-        text.printTitle();
-        
-        System.out.println("1.Start game" + "\n" + "2.View Achievements" + "\n" + "3.Continue Game");
-        int choice = input.nextInt();
-        
-      
-        
-        
-
-        if(choice == 1)
-        {
-          //Start new game, selection process needs to be streamlined
-          
-          
-        }
-
-        else if(choice == 2)
-        {
-          //Achievements menu
-        }
-        //continue game
-        else if(choice == 3)
-        {
-          
-        }
-        else
-        {
-          System.out.println("Not a valid input. Please try again");
-        }
-        
-       
+    public static void main(String[] ars) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Menu Page");
+    System.out.println("Enter the name you want for your character");
+     String name = input.nextLine();
+    Quests questSelector = new Quests(name);
+      for(int i = 1; i < 3; i++){
+        questSelector.runQuest(i);
+      }
     }
 }
