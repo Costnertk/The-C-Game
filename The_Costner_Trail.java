365e687ed26cf5f4af9b92c3e7b9d4e49b028031
Costner Magill <costnerxman360@gmail.com>
2019-12-07 01:12:19 -0500

    began main menu logic tree

--- The_Costner_Trail.java
+++ The_Costner_Trail.java
@@ -7,10 +7,55 @@ public class The_Costner_Trail
         System.out.println("Menu Page");
         System.out.println("Enter the name you want for your character");
         String name = input.nextLine();
+        String userInput;
 
         StartGame game1 = new StartGame(name);
 
         game1.startGame();
         
+        while(true)
+        {
+            // main menu while loop
+            
+            // Images needs to be implimented to print main menu
+
+            // primts out menu options
+            System.out.println("1.Start game" + "\n2.Continue Game" + "\n3.Load Game" + "\n4.Quit Game");
+            userInput = input.nextLine();
+
+            // Logic trees for menu
+
+            // Start game
+            if(userInput.equals("1"))
+            {
+
+            }
+
+            // Continue Game
+            else if(userInput.equals("2"))
+            {
+
+            }
+
+            // Load Game 
+            else if(userInput.equals("3"))
+            {
+
+            }
+
+            // loop should only break when user choses to quit game
+
+            else if(userInput.equals("4"))
+            {
+                break;
+            }
+
+            else
+            {
+                System.out.println("Not a valid input. Please try againg.");
+            }
+
+
+        }
     }
 }
